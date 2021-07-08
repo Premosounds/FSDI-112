4e8aa8283f47169cb4d49b780e877778eacb8845
Jay Boyd <jamesboyd3rd@gmail.com>
2021-07-08 08:09:05 +0700

    Added deployment dependecies

---
+++ Procfile
@@ -0,0 +1,1 @@
+wed: gunicorn config.wsgi --log-file -
--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5ba4_vl9v2n8m&^f7jg5tsvsb+%_n7ffq-!pe&2ycru_4a_cee
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "herokuapp.com"]
 
 
 # Application definition
